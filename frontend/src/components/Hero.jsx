ce96712dbe4e5d57239f86ca5ffc352979655620
Sam <soham.mhatre97@gmail.com>
2025-03-06 12:24:03 -0500

    Fixed Black Box

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,12 +1,11 @@
-import BgVideo from "../assets/bg_video.mp4";
 import { useState } from "react";
 import { useNavigate } from "react-router";
-
+import BgVideo from "../assets/bg_video.mp4";
 const Hero = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const navigate = useNavigate();
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
     const urlParams = new URLSearchParams(window.location.search);
     urlParams.set("searchTerm", searchTerm);
@@ -17,26 +16,28 @@ const Hero = () => {
 
   return (
     <div className="relative h-[75vh] lg:h-[90vh] text-[#FFFCF2] px-4 md:px-12 overflow-hidden">
-        <div className="bg-[#252422] w-full h-full absolute top-0 left-0 opacity-80 -z-10">
-            {}
-            <div></div>
-            <div>
-                <video className="object-cover object-center w-full h-full" autoPlay loop muted>
-                    <source src={BgVideo} type="video/mp4" />
-                </video>
-            </div>
-        </div>
-        <div className="w-full h-full flex flex-col justify-center items-center !z-50">
-            <h1 className="text-3xl md:text-4xl lg:text-6xl pb-8 lg:pb-12 text-center max-w-5xl -mt-20">
-            Share your <span className="text-[#EB5E28]">favorite</span> books and{" "}
-            <span className="text-[#EB5E28]">discover</span> new ones from readers
-            like you.
-            </h1>
+      <div className="bg-[#252422] w-full h-full absolute top-0 left-0 opacity-80 -z-10"></div>
+      <div className="absolute inset-0 -z-20">
+        <video
+          className="object-cover object-center w-full h-full"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={BgVideo} type="video/mp4" />
+        </video>
+      </div>
+      <div className="w-full h-full flex flex-col justify-center items-center !z-50">
+        <h1 className="text-3xl md:text-4xl lg:text-6xl pb-8 lg:pb-12 text-center max-w-5xl -mt-20">
+          Share your <span className="text-[#EB5E28]">favorite</span> books and{" "}
+          <span className="text-[#EB5E28]">discover</span> new ones from readers
+          like you.
+        </h1>
 
         <form
           onSubmit={handleSubmit}
           className="relative w-full max-w-sm md:max-w-xl lg:max-w-3xl text-base lg:text-lg"
-            >
+        >
           <input
             type="text"
             value={searchTerm}
@@ -51,10 +52,9 @@ const Hero = () => {
             Search
           </button>
         </form>
-
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
