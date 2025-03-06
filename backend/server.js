eba1d2d62978aa9678ab2c3cdb289b662905e54b
Sam <soham.mhatre97@gmail.com>
2025-03-06 12:02:59 -0500

    Updated URL

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const __dirname = path.resolve();
 
 console.log("PORT is", process.env.PORT)
 
-app.use(cors({origin:"http://localhost:5174", credentials: true}))
+app.use(cors({origin:"https://bookrentingapp.onrender.com", credentials: true}))
 app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://bookrentingapp.onrender.com/api";
 axios.defaults.withCredentials = true;
 
 
